15e2cf82308730665808e38893a4942c2ea92942
desktopjs1622 <cristoferjcolmenares@gmail.com>
2019-03-04 10:30:31 -0400

    cambiando estilo de bootstrap a materialize-bootstrap

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -4,25 +4,11 @@ from cuenta.views import Login
 
 app_name = 'cuenta'
 urlpatterns = [
-    path( # Iniciar sesion utilizando materialize
-        'login/', 
+    path( # Iniciar sesion, Registro de Usuario y Recuperar Cuenta
+        'inicio/', 
         Login.as_view(
             template_name='login.html'
         ), 
         name='inicio-sesion'
-    ),
-    path(#Registro de Nuevos Usuarios
-        'login/nuevo-registro/',
-        Login.as_view(
-            template_name='registro_usuario.html'
-        ), 
-        name='registro-de-usuario'
-    ),
-    path(
-        'login/recuperar/', 
-        Login.as_view(
-            template_name='recuperar_cuenta.html'
-        ), 
-        name='recuperar-cuenta'
     ),
 ]
