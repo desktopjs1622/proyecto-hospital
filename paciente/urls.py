44cd3afa47b8f84fd8edeab30148c67a47251c5e
desktopjs1622 <cristoferjcolmenares@gmail.com>
2019-03-10 13:47:31 -0400

    depurando las librerias

--- paciente/urls.py
+++ paciente/urls.py
@@ -1,22 +1,48 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from paciente.views import ListadoPaciente, RegistroPaciente
 
 app_name = 'paciente'
 urlpatterns = [
     path( #listar
-        'listado_de_paciente/', 
+        'listado/', 
         ListadoPaciente.as_view(
             template_name = 'paciente_listar.html',
             extra_context = {'titulo': 'Listado General', 'title':'Historias Médicas'},
         ), 
         name='lista-paciente'
     ),
-    path(#agregar
-        'registro-de-paciente/',
+    path( #Detalle
+        'listado/detalle', 
+        TemplateView.as_view(
+            template_name='paciente_detalle.html',
+            extra_context = {'titulo': 'Paciente'}
+        ), 
+        name='detalle-paciente'
+    ),
+    path( #Eliminar
+        'listado/eliminar', 
+        TemplateView.as_view(
+            template_name='paciente_eliminar.html',
+            extra_context = {'titulo': 'Paciente'}
+        ), 
+        name='eliminar-paciente'
+    ),
+    path( #agregar
+        'registro/agregar',
         RegistroPaciente.as_view(
             template_name='paciente_formulario.html',
             extra_context={'titulo': 'Registro', 'title': 'Registro de Paciente'},
         ), 
         name='registro-paciente'
     ),
+    path( #agregar
+        'registro/editar',
+        RegistroPaciente.as_view(
+            template_name='paciente_formulario.html',
+            extra_context={'titulo': 'Registro', 'title': 'Registro de Paciente'},
+        ), 
+        name='editar-paciente'
+    ),
+    
 ]
